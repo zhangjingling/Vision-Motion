7b10179259a540906415bb97993adf21bf7020d1
JimJarvis <jimfanspire@gmail.com>
2014-04-28 18:18:17 -0400

    Complete computeFlow with normxcorr2

--- computeFlow.m
+++ computeFlow.m
@@ -20,11 +20,11 @@ highx = @(x) min(WX, x + Wr);
 highy = @(y) min(WY, y + Wr);
 
 % Get the extension to all directions
-function ext = getExt(x, y)
-    lx = min(x - 1, Tr);
-    ly = min(y - 1, Tr);
-    rx = min(WX - x, Tr);
-    ry = min(WY - y, Tr);
+function ext = getExt(x, y, r)
+    lx = min(x - 1, r);
+    ly = min(y - 1, r);
+    rx = min(WX - x, r);
+    ry = min(WY - y, r);
     ext = [lx rx ly ry];
 end
 
@@ -33,41 +33,37 @@ function win = getWin(img, x, y, ext)
     win = img(y-ext(3):y+ext(4), x-ext(1):x+ext(2));
 end
 
+vec = zeros(2, 1);
 for i = 1:numel(X) 
     for j = 1:numel(Y) 
         x = X(i);
         y = Y(j);
-        % original window
-        ext = getExt(x, y);
-        maxCorr = 0;
-        maxXY = [0 0];
-
-        % Search the window
-        for x_ = max(1, x-Wr) : min(WX, x+Wr)
-            for y_ = max(1, y-Wr) : min(WY, y+Wr)
-                ext_ = min(ext, getExt(x_, y_));
-                win = getWin(img1, x, y, ext_);
-                win_ = getWin(img2, x_, y_, ext_);
-
-                corr = corr2(win, win_);
-                % update the maximum so far
-                if corr > maxCorr
-                    maxCorr = corr;
-                    maxXY = [x_ y_];
-                end
-            end
-        end
+        % original template
+        tmpl_ext = getExt(x, y, Tr);
+        tmpl = getWin(img1, x, y, tmpl_ext);
+        % search window
+        search_ext = getExt(x, y, Wr+Tr);
+        search = getWin(img2, x, y, search_ext);
+        corr = normxcorr2(tmpl, search);
+        [cval, maxXY] = max(corr(:));
+        % Discard a low correlation
+        if cval < 0.5, continue, end
+        [maxY maxX] = ind2sub(size(corr), maxXY);
 
         % Store the displacement to the velocity map
-        U(j, i) = maxXY(1) - x;
-        V(j, i) = maxXY(2) - y;
+        vec(1) = -search_ext(1) + maxX-1 - tmpl_ext(2);
+        vec(2) = -search_ext(3) + maxY-1 - tmpl_ext(4);
+        if norm(vec) < WX/GX + WY/GY
+            U(j, i) = vec(1);
+            V(j, i) = vec(2);
+        end
     end
 end
 
 %% Generate the annotated image with quiver
 [X Y] = meshgrid(X, Y);
 
-fh = figure; imshow(img1), hold on, quiver(X, Y, U, V);
+fh = figure; imshow(img1), hold on, quiver(X, Y, U, V, 'autoscale', 'off');
 
 result = saveAnnotatedImg(fh);
 end
